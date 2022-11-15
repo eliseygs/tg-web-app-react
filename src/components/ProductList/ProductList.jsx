466fe9410a49fc5654d215b1feb9a80fd84d8faa
b <a>
2022-11-15 19:56:51 +0300

    add

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -30,7 +30,7 @@ const ProductList = () => {
             },
             body: JSON.stringify(data)
         })
-    },[])
+    },[addedItems])
 
     useEffect(()=>{
         tg.onEvent('mainButtonClicked', onSendData)
